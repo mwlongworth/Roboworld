65c33f0e1caa4f4b9f98617e56250fed7be17894
Matthew Longworth <mlongworth@mattl-pc.levelb.lan>
2016-02-21 14:20:15 +0000

    Add first project

---
+++ Roboworld.Recipes.WebApi/App_Start/Startup.cs
@@ -0,0 +1,23 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Startup.cs" company="">
+//   
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+[assembly: Microsoft.Owin.OwinStartup(typeof(Roboworld.Recipes.WebApi.Startup))]
+
+namespace Roboworld.Recipes.WebApi
+{
+    using System.Web.Http;
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
---
+++ Roboworld.Recipes.WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿namespace Roboworld.Recipes.WebApi
+{
+    using System;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Net.Http.Formatting;
+    using System.Web.Http;
+
+    using Newtonsoft.Json.Serialization;
+
+    using Swashbuckle.Application;
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.EnableSwagger(
+                c =>
+                {
+                    c.SingleApiVersion("v1", "Resource Service API");
+                    c.RootUrl(req => new Uri(req.RequestUri, req.GetRequestContext().VirtualPathRoot).ToString());
+                }).EnableSwaggerUi();
+        }
+    }
+}
